c54be075d88a5c5c5ed34b6284af6cab62601a9f
MichaelStevenson2207 <wubblyjuggly@hotmail.co.uk>
2023-06-02 21:18:47 +0100

    Updated to .net 7

--- Controllers/DeveloperService.cs
+++ Controllers/DeveloperService.cs
@@ -1,9 +1,9 @@
-﻿using edd_blazor_server_poc.Data;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using edd_blazor_server_poc.Data;
 using edd_blazor_server_poc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace edd_blazor_server_poc.Controllers
 {
--- Program.cs
+++ Program.cs
@@ -1,20 +1,38 @@
-using Microsoft.AspNetCore.Hosting;
+using edd_blazor_server_poc.Controllers;
+using edd_blazor_server_poc.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace edd_blazor_server_poc
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<DeveloperService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
